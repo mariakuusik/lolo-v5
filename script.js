69b6b277771efaf0883f41901b368dac114000c0
mariakuusik <maria.kuusik@gmail.com>
2024-06-19 14:14:22 +0300

    add: fetched images of initial content

--- script.js
+++ script.js
@@ -25,9 +25,15 @@ function renderArticles(articles) {
     content.innerHTML = '';
 
     articles.forEach(item => {
+        console.log(JSON.stringify(item, null, 2));
+
         const article = document.createElement('div');
         article.className = 'article';
+
+        const imageUrl = item.enclosure ? item.enclosure.link : '';
+
         article.innerHTML = `
+            ${imageUrl ? `<img src="${imageUrl}" alt="${item.title}" class="article-image" width="300">` : ''}
             <h2><a href="${item.link}" target="_blank">${item.title}</a></h2>
             <p>${item.description}</p>
             <small>${new Date(item.pubDate).toLocaleString()}</small>
